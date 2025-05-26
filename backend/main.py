6648bae7367bd5e628dafa6c9553b53d54d217db
Rhithanya <rhithanyaparthasarathi@gmail.com>
2025-05-26 12:39:09 +0530

    added another frontend origin

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:5500",
-                   "https://chroni-care-ez3l.vercel.app"],  # ONLY allow your frontend origin
+                   "https://chroni-care-ez3l.vercel.app",
+                   "https://chroni-care.vercel.app"],  # ONLY allow your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
